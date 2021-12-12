cd30838950e1cc873054d139de4b84b4dd811051
Nguimeya Bill-gates <bnguimeya007@gmail.com>
2021-12-12 21:17:55 +0100

    fix movieDetail page

--- pages/movie/[slug].tsx
+++ pages/movie/[slug].tsx
@@ -6,27 +6,36 @@ import IMovie from "../../types/IMovie";
 import Image from 'next/image'
 import { ReactChild, ReactFragment, ReactPortal, Key } from "react";
 
-export default function MovieDetail(movie: IMovie ): JSX.Element {       
+export default function MovieDetail(movie: IMovie): JSX.Element {
     return (
         <>
             <Header />
-            <div className="bg-[#acacac]">
-                <div className="customHeight container mx-auto pt-20 px-4 min-h-screen pb-28">
-                    <div className="flex flex-col pt-10">
-                    <div className="relative image-container aspect-h-1 aspect-w-1 sm:aspect-h-16 sm:aspect-w-9 md:h-40">
-                        <Image layout="fill" objectFit="cover" alt={movie.slug} src={movie.poster} className="poster-image" />
-                    </div>
-                    <div>
-                        <h1 className="font-bold text-2xl text-center">{movie.title}</h1>
-                        <div className="flex gap-2 mt-2">
-                        {movie.cast.map((e: boolean | ReactChild | ReactFragment | ReactPortal | null | undefined, index: Key | null | undefined) => <p key={index} className="inline">{e}</p>)}
+            <div className="bg-[#acacac] min-h-screen">
+                <div className="container flex mx-auto pt-20 px-4 pb-28 justify-around">
+                    <div className="flex flex-col pt-10 md:flex-row md:h-full md:items-center md:justify-center md:w-screen md:gap-6 xl:gap-8">
+                        <div className="mb-4">
+                            <h1 className="font-bold text-2xl md:text-3xl xl:text-4xl max-w-sm">{movie.title}</h1>
+                            <div className="my-2 flex gap-1 md:text-lg"> <p>{movie.released_on.slice(0, 4)}</p><span>&#183;</span><p className="text-[12px] bg-black text-white rounded-full p-1">{movie.classification}</p><span>&#183;</span><p>{movie.length}</p> </div>
+                            <div className="relative image-container aspect-h-16 aspect-w-9 md:h-40">
+                                <Image layout="fill" objectFit="cover" alt={movie.slug} src={movie.poster} className="poster-image" />
+                            </div>
                         </div>
-                        <div className="max-w-md mt-2">
-                            <p>{movie.overview}</p>
+                        <div className="flex flex-col text-xl xl:text-2xl">
+                            <div>
+                                <div className="flex gap-2 my-4">
+                                    {movie.genres.map((e, index) => <p className="text-[14px] border border-black italic font-bold rounded-full py-1 px-2" key={index}>{e}</p>)}
+                                </div>
+                                <p>Director: {movie.director}</p>
+                                <div className="flex gap-2 mt-1">
+                                    <p>Cast: {movie.cast.join(", ")}</p>
+                                </div>
+                                <div className="max-w-md mt-2">
+                                    <p>{movie.overview}</p>
+                                    <p className="text-sm md:text-lg bg-yellow-300 inline pr-1">Rating: {movie.imdb_rating}</p>
+                                </div>
+                            </div>
                         </div>
-                        <div className="mt-2"> <p>{movie.released_on}</p> <p>{movie.length}</p> <p>{movie.director}</p></div>
-                        
-                    </div>
+
                     </div>
                 </div>
             </div>
@@ -35,24 +44,22 @@ export default function MovieDetail(movie: IMovie ): JSX.Element {
     )
 }
 
-export async function getStaticProps(params: { slug: string; }) {                   
-    const movie = await searchApi(params.slug).then(res => res.data.movies[0]);    
-    console.log(params.slug);
-    
-    return {props:  movie}    
+export async function getStaticProps(params: { params: { slug: string; } }) {
+    const movie = await searchApi(params.params.slug).then(res => res.data.movies[0]);
+    return { props: movie }
 }
 
 export async function getStaticPaths() {
     const movies = await moviesApi.then(res => res.data.movies);
     const paths = movies.map((movie: IMovie) => {
         return {
-                params:
-                    {
-                    slug: movie.slug
-                }
+            params:
+            {
+                slug: movie.slug
             }
-        })                
+        }
+    })
     return {
-        paths , fallback: false
+        paths, fallback: false
     }
 }
